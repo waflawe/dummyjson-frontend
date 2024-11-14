a37c5d3542c56bf44ff60ae41e7b4219d17c0054
waflawe <popejpivaoleg@gmail.com>
2024-11-14 18:39:18 +0900

    Update navbar styling

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,11 +1,13 @@
 <template>
-  <nav class="fixed top-0 left-0 w-full h-14 bg-slate-100 bg-opacity-100 shadow-xl">
-    <div class="h-full grid grid-cols-12 place-items-center">
-      <RouterLink :to="{name: 'home'}" class="text text-2xl col-span-2">
-        <p class="sawarabi">dummyJSON</p>
+  <nav class="fixed top-0 left-0 w-full h-14 bg-slate-100 bg-opacity-100 shadow-xl dark:bg-slate-900">
+    <div class="h-full place-items-center flex">
+      <RouterLink :to="{name: 'home'}" class="ms-10 text text-dark text-2xl">
+        <p class="sawarabi flex"><img width="30" height="30" src="https://img.icons8.com/fluency/30/pixel-heart.png" alt="pixel-heart"/>dummyJSON</p>
       </RouterLink>
-      <div class="col-span-8"></div>
-      <div class="col-span-2">
+      <RouterLink :to="{name: 'products'}" class="ms-10 text text-dark text-2xl">
+        <p class="sawarabi">Products</p>
+      </RouterLink>
+      <div class="ms-auto me-10">
         <RouterLink :to="{name: 'login'}">
           <button class="nav-btn text-lg" type="button">Login</button>
         </RouterLink>
